d7241bcda9e3de04e80fc5359aa8f163ca04ebe9
hongseo1 <hongseow109@gmil.com>
2024-04-08 14:07:43 +0900

    240408

---
+++ project06/src/components/WishList.vue
@@ -0,0 +1,41 @@
+<!--WishList.vue-->
+<template lang="html">
+    <section>
+        <ul>
+            <li v-for="wishItem in wishItems" class="shadow">
+                <i class="checkBtn fas fa-check" aria-hidden="turn"></i>
+                {{wishItem}}
+                <span class="removeBtn" type="button" @click="removeWish">
+                    <i class="far fa-trash-alt" aria-hidden="true"></i>
+                </span>
+            </li>
+        </ul>
+    </section>
+</template>
+<script>
+    export default{
+        data(){
+            return{
+                wishItems: []
+            }
+        },
+        methods:{
+            removeWish(){
+                console.log('clicked');
+            }
+        },
+        created(){
+            if(localStorage.length>0){
+                for(var i=0; i<localStorage.length; i++){
+                    this.wishItems.push(localStorage.key(i));
+                }
+            }
+        }
+    }
+</script>
+<style lang="css" scoped>
+    ul{list-style-type: none; padding-left: 0; margin-top: 0; text-align: left;}
+    li{display: flex; min-height: 50px; height: 50px; line-height: 50px; margin: 0.5em 0; padding: 0 0.9em; background: #fefefe; border-radius: 5px; overflow: hidden; text-overflow: ellipsis;}
+    .checkBtn{line-height: 50px; color: #ff8000; margin-right: 5px;}
+    .removeBtn{margin-left: auto; color: #de4343;}
+</style>
