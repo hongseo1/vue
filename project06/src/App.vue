1903a93dd8a3835369f00ca18f940e5ff51b4bc8
hongseo1 <hongseow109@gmil.com>
2024-04-11 09:50:15 +0900

    24

--- project06/src/App.vue
+++ project06/src/App.vue
@@ -3,8 +3,8 @@
         <WishHeader></WishHeader>
         <WishInput v-on:addWish="addWish"></WishInput>
         <!--v-on:addWish에 addWish는 이벤트(하위에서 함수 호출하는 이름), = "addWish"에 addWish는 App.vue에 정의한 함수-->
-        <WishList v-bind:propsData="wishItems" @removeWish="removeWish"></WishList>
-        <WishFooter v-on:removeAll="clearAll"></WishFooter>
+        <WishList v-bind:propsData="wishItems" @removeWish="removeWish" @aditWish="editWish"></WishList>
+        <WishFooter v-on:removeAllOfChild="removeAllOfParents"></WishFooter>
     </div>
 </template>
 <script>
@@ -20,17 +20,46 @@
             }
         },
         created(){
-            if(localStorage.length>0){
-                for(var i=0; i<localStorage.length; i++){
-                    this.wishItems.push(localStorage.key(i));
-                }
+            if(localStorage.getItem("vue-whish"){
+                this.wishItems = JSON.parse(localStorage.getItem(vue-wish))
+                this.wishItems.sort(function(a,b){
+                    return a.key < b.key ? -1 : a.key > b.key ? 1 : 0;
+                    //배열 정리 시 사용, if문은 사용할 수 없기때문에 3항 연산자 사용
+
+                    /* 예문
+                    var arr = [{key:3}], [{key:1}], [{key:2}];
+                    arr.sort(function(a,b){
+                        return a.key < b.key ? -1 : a.key > b.key ? 1 : 0;
+                    });
+                    console.log(arr);
+                    */
+                    // 결과 [{key:1}], [{key:2}], [{key:1}]
+                });
             }
         },
         methods:{
-            addWish(wishItem){
-                //로컬 스토리지에 데이터를 추가하는 로직
-                localStorage.setItem(wishItem, wishItem);
-                this.wishItems.push(wishItem);
+            addWish(key, value, date){
+                this.wishItems.push({
+                    key,
+                    value,
+                    createdDate: date,
+                    modifiedDate: date
+                })
+                localStorage.setItem("vue-wish", JSON.stringify(this.wishItems))
+                /* 예문
+                    let car = {
+                        no : "11가 1111",
+                        name : "소나타",
+                        maker : "현대",
+                        cc : 2000,
+                        year: 2021
+                    };
+                */
+                /* 자바스크립트 객체를  JSON 데이터로 변환
+                    var json = JSON.stringify(car);
+                    console.log(json);
+                */
+                // 결과 {"no":"11가 1111", "name":"소나타", "maker":"현대", "cc":2000, "year":2021}
             },
             clearAll(){
                 localStorage.clear();
